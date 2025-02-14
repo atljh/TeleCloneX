821cfd575082ff8982dbfa3e3c683a909eb0767c
Fyodor <fiodorlukashov@gmail.com>
2025-02-14 22:51:50 +0200

    Add main file

---
+++ main.py
@@ -0,0 +1,21 @@
+import asyncio
+from src.starter import Starter
+from config import ConfigManager, print_config
+from src.thon.json_converter import JsonConverter
+# from src.managers.file_manager import FileManager, groups
+
+
+async def run_starter(sessions_count, config):
+    starter = Starter(sessions_count, config)
+    await starter.main()
+
+
+def main():
+    config = ConfigManager.load_config()
+    print_config(config)
+    sessions_count = JsonConverter().main()
+    asyncio.run(run_starter(sessions_count, config))
+
+
+if __name__ == "__main__":
+    main()
